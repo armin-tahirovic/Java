d419d6e494392de014ebaea7153e37f609d587ba
armin <armin2013@live.dk>
2018-12-10 11:09:02 +0100

    Initial commit

---
+++ src/Address.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+
+public class Address {
+    private String address;
+
+
+    ArrayList<String> addresses = new ArrayList<>();
+
+    public Address() {
+        Address address = new Address();
+    }
+
+    public void addAddress(String address) {
+        addresses.add(address);
+    }
+}
