a57a4840ce621332529cb972328aeb184f32dc6e
armin <armin2013@live.dk>
2018-12-13 13:31:54 +0100

    Changes 5

---
+++ src/model/Login.java
@@ -0,0 +1,23 @@
+package model;
+
+public class Login {
+
+
+    public boolean checkSuperUser(int apartmentNumber, Domain apartment, int userNumber) {
+
+        if (apartment.apartmentList.get(apartmentNumber).getUserList().get(userNumber).isSuperUser()) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public boolean checkUserPw(int apartmentNumber, Domain apartment, int userNumber, String userPw) {
+
+        if (apartment.apartmentList.get(apartmentNumber).getUserList().get(userNumber).getPw() == userPw) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
